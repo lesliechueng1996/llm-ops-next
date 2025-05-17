7bf18fe67c61067fdbf6f2387445497f037ffecb
Leslie <lesliechueng1996@icloud.com>
2025-05-17 20:21:26 +0800

    add api key plugin in better auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,11 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '@/lib/db';
+import { apiKey } from 'better-auth/plugins';
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: 'pg',
   }),
+  plugins: [apiKey()],
 });
